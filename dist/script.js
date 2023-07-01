7076692a1644a57c1c91b7fdcb9dfa1777301579
goeunpark <goeuntothepark@gmail.com>
2023-07-01 14:56:49 -0700

    Add fake data for demo

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,43 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 console.log('♡ ^•ﻌ•^ฅ ♡');
 console.log('meow! meow!');
 console.log('♡ ^•ﻌ•^ฅ ♡');
+document.addEventListener('DOMContentLoaded', init, false);
+const dummyData = [
+    {
+        name: 'Sleeping bag',
+        description: 'good enough',
+        status: true,
+    },
+    {
+        name: 'Stove',
+        description: 'eat that food',
+        status: true,
+    },
+];
+function init() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let table = document.querySelector('#gearTable tbody');
+        let result = '';
+        dummyData.forEach((gear) => {
+            result += `<tr>
+        <td>${gear.name}</td>
+        <td>1</td>
+        <td>${gear.description}</td>
+        <td>${gear.status}</td>
+        </tr>`;
+        });
+        if (table) {
+            table.innerHTML = result;
+        }
+    });
+}
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,41 @@
 console.log('♡ ^•ﻌ•^ฅ ♡')
 console.log('meow! meow!')
 console.log('♡ ^•ﻌ•^ฅ ♡')
+
+document.addEventListener('DOMContentLoaded', init, false)
+
+interface GearItem {
+    name: String
+    description: String
+    status: boolean
+}
+
+const dummyData: GearItem[] = [
+    {
+        name: 'Sleeping bag',
+        description: 'good enough',
+        status: true,
+    },
+    {
+        name: 'Stove',
+        description: 'eat that food',
+        status: true,
+    },
+]
+
+async function init() {
+    let table: HTMLElement | null = document.querySelector('#gearTable tbody')
+
+    let result = ''
+    dummyData.forEach((gear) => {
+        result += `<tr>
+        <td>${gear.name}</td>
+        <td>1</td>
+        <td>${gear.description}</td>
+        <td>${gear.status}</td>
+        </tr>`
+    })
+    if (table) {
+        table.innerHTML = result
+    }
+}
