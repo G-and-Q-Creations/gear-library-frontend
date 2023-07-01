6eb0e905d1158c3dd9c2fccf216300caeff341df
goeunpark <goeuntothepark@gmail.com>
2023-06-30 22:33:34 -0700

    Sync up pnpm run dev to ts-node instead of node

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('goeun & quigley gear library!');
+    res.send('goeun and quigley gear library!');
 });
 
 app.listen(port, () => {
